6b414e98b5db10864cd13814f85ee808cff63b43
Samuel Krieg <samuel.krieg+github@gmail.com>
2023-12-05 11:40:46 +0100

    perf!: rm task compose.validate
    Tu fais compose.show > /dev/null et c reglé

--- swarm_app/compose.py
+++ swarm_app/compose.py
@@ -1,9 +1,8 @@
 """
 Manipule le fichier docker-compose.yml
 """
-import sys
 from invoke import task
-from swarm_app import env_files, docker
+from swarm_app import docker
 
 
 @task
@@ -11,18 +10,3 @@ def show(c):
     """Affiche le fichier docker-compose.yml rendered"""
     client = docker.Docker(c)
     client.show()
-
-
-@task
-def validate(c):
-    """Validate docker-compose.yml"""
-    cmd_prefix = env_files.cmd_prefix(c.env)
-    command = f"{cmd_prefix} docker-compose config --quiet"
-    ignore = ["Compose does not support 'configs' configuration"]
-
-    result = c.run(command, hide="stderr")
-    for line in result.stderr.split("\n"):
-        if any(word in line for word in ignore):
-            pass
-        else:
-            print(line, file=sys.stderr)
