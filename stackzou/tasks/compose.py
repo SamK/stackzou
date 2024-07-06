a986d341cedb029b8881f161bc21957b63546746
Samuel Krieg <samuel.krieg+github@gmail.com>
2024-07-06 12:54:04 +0200

    feat: add --skip-interpolation

--- stackzou/tasks/compose.py
+++ stackzou/tasks/compose.py
@@ -7,7 +7,9 @@ from stackzou import docker
 
 
 @task
-def show(c):
+def show(c, skip_interpolation=False):
     """Render and print the docker-compose.yml file"""
     client = docker.Docker(c)
+    if skip_interpolation:
+        client.stack_args += " --skip-interpolation"
     client.show()
